4e8ccf8fa6529334fa307ad02162bbe2d312c310
Arnelle Balane <arnellebalane@gmail.com>
2019-01-27 00:26:23 +0800

    Define gulp tasks for building assets

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+
+gulp.task('build:css', () => {
+    return gulp.src('static-root/demoproject/css/index.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('static-root/build/demoproject/css'));
+});
+
+gulp.task('build:js', () => {
+    return gulp.src([
+            'static-root/demoproject/js/index.js',
+            'static-root/demoproject/js/require-config.js'
+        ])
+        .pipe(uglify())
+        .pipe(gulp.dest('static-root/build/demoproject/js'));
+});
+
+gulp.task('build:vendor', () => {
+    return gulp.src('static-root/vendor/**/*')
+        .pipe(gulp.dest('static-root/build/vendor'));
+});
+
+gulp.task('build', gulp.series('build:css', 'build:js', 'build:vendor'));
