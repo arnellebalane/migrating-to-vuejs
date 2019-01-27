c150b976ff8fd3e16ee7c7c832a45e92b85e362d
Arnelle Balane <arnellebalane@gmail.com>
2019-01-27 12:25:00 +0800

    Refactor videoplayer progress to use css animations

--- demoproject/static/demoproject/js/components/video-player.js
+++ demoproject/static/demoproject/js/components/video-player.js
@@ -4,18 +4,23 @@ define(require => {
     class VideoPlayer {
         constructor(element) {
             this.element = Zepto(element);
-            this.overlay = this.element.find('.video-player__overlay');
             this.progress = this.element.find('.video-player__progress');
 
             this.playing = false;
-            this.percentage = 0;
-            this.intervalID = null;
 
             this._initializeInteraction();
+            this._initializeEvents();
         }
 
         _initializeInteraction() {
             this.element.on('click', () => this.toggle());
+        }
+
+        _initializeEvents() {
+            this.progress.on('animationend', () => {
+                this.pause();
+                this.progress.removeClass('animating');
+            });
         }
 
         toggle() {
@@ -28,25 +33,13 @@ define(require => {
 
         play() {
             this.playing = true;
-            this.overlay.addClass('playing');
-
-            if (this.percentage === 100) {
-                this.percentage = 0;
-            }
-
-            this.intervalID = setInterval(() => {
-                this.progress.css('width', `${++this.percentage}%`);
-                if (this.percentage === 100) {
-                    this.pause();
-                }
-            }, 250);
+            this.element.addClass('playing');
+            this.progress.addClass('animating');
         }
 
         pause() {
             this.playing = false;
-            this.overlay.removeClass('playing');
-
-            clearInterval(this.intervalID);
+            this.element.removeClass('playing');
         }
     }
 
