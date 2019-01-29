6e9e9f6a8328c7b494a36549445af43e8f00c671
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 16:46:31 +0800

    4.2.1: Define VideoPage component

---
+++ demoproject/static/demoproject/vue/pages/VideoPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <h1>Video Page</h1>
+
+        <div id="video-container"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+    padding: 60px 0;
+}
+
+h1 {
+    margin-top: 0;
+    margin-bottom: 30px;
+}
+</style>
