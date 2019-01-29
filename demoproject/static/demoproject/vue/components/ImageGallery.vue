56bdb7b80d7ea7bad9357dc9c47972811c3f34ae
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 13:37:32 +0800

    2.4.1: Move gallery markup and styles to ImageGallery component

---
+++ demoproject/static/demoproject/vue/components/ImageGallery.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="gallery">
+        <img src="https://picsum.photos/800/400/?image=14">
+        <img src="https://picsum.photos/400/400/?image=13">
+        <img src="https://picsum.photos/400/400/?image=12">
+        <img src="https://picsum.photos/400/400/?image=11">
+        <img src="https://picsum.photos/400/400/?image=10">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+img:not(:first-child) {
+    width: calc((100% - 24px) / 2);
+    height: 400px;
+    object-fit: cover;
+    object-position: center center;
+    margin-top: 24px;
+}
+</style>
--- demoproject/static/demoproject/vue/pages/GalleryPage.vue
+++ demoproject/static/demoproject/vue/pages/GalleryPage.vue
@@ -2,15 +2,19 @@
     <div class="wrapper">
         <h1>Gallery Page</h1>
 
-        <div class="gallery">
-            <img src="https://picsum.photos/800/400/?image=14">
-            <img src="https://picsum.photos/400/400/?image=13">
-            <img src="https://picsum.photos/400/400/?image=12">
-            <img src="https://picsum.photos/400/400/?image=11">
-            <img src="https://picsum.photos/400/400/?image=10">
-        </div>
+        <ImageGallery />
     </div>
 </template>
+
+<script>
+import ImageGallery from '../components/ImageGallery.vue';
+
+export default {
+    components: {
+        ImageGallery
+    }
+};
+</script>
 
 <style lang="scss" scoped>
 .wrapper {
@@ -20,19 +24,5 @@
 h1 {
     margin-top: 0;
     margin-bottom: 30px;
-}
-
-.gallery {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-}
-
-img:not(:first-child) {
-    width: calc((100% - 24px) / 2);
-    height: 400px;
-    object-fit: cover;
-    object-position: center center;
-    margin-top: 24px;
 }
 </style>
