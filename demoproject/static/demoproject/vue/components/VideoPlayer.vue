1e0207b1dd6c14d8651297fa3a7c4fbb55cfe855
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 16:50:52 +0800

    4.3.1: Define VideoPlayer component

---
+++ demoproject/static/demoproject/vue/components/VideoPlayer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="video-player">
+        <div class="video-player__overlay"></div>
+        <div class="video-player__track">
+            <div class="video-player__progress"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped src="css/_player.scss"></style>
