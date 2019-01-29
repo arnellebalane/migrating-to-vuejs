5da468823022ea4d715e2cd042a7c009314e2958
Arnelle Balane <arnellebalane@gmail.com>
2019-01-30 00:17:41 +0800

    5.7.1: Remove VueRouter from TheHeader adapter

--- demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
+++ demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
@@ -1,17 +1,8 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import store from '../store';
 import TheHeader from '../components/TheHeader.vue';
 
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: []
-});
-
 export default new Vue({
-    router,
     store,
     render: h => h(TheHeader)
 });
