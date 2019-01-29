c2765d12c505099edf9999509e894340de090ca1
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 13:53:30 +0800

    3.1.2: Render ThePopup in App.vue

--- demoproject/static/demoproject/vue/App.vue
+++ demoproject/static/demoproject/vue/App.vue
@@ -2,15 +2,18 @@
     <div>
         <TheHeader />
         <router-view></router-view>
+        <ThePopup />
     </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
+import ThePopup from './components/ThePopup.vue';
 
 export default {
     components: {
-        TheHeader
+        TheHeader,
+        ThePopup
     }
 };
 </script>
