37466773b338ca6c72dad95f6b1693fc8677bae3
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 11:53:33 +0800

    1.3.1: Add webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import VueLoaderPlugin from 'vue-loader/lib/plugin';
+
+export default {
+    entry: path.resolve(__dirname, 'static-root/demoproject/vue/index.js'),
+
+    output: {
+        path: path.resolve(__dirname, 'static-root/build/demoproject/vue'),
+        filename: 'index.js',
+        chunkFilename: '[name].js',
+        publicPath: 'http://localhost:8081/build/demoproject/vue/'
+    },
+
+    mode: process.env.NODE_ENV || 'development',
+
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
