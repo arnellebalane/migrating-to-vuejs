0675098ca94ec64154625674248f063f0391c8ff
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 12:45:28 +0800

    1.7.2: Define top-level App.vue component

---
+++ demoproject/static/demoproject/vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header class="header">
+            <div class="header__content wrapper">
+                <a href="/" class="open-page">
+                    <h1 class="header__title">DemoProject</h1>
+                </a>
+
+                <nav class="nav">
+                    <a href="/image" class="nav__link open-page">Image</a>
+                    <a href="/gallery" class="nav__link open-page">Gallery</a>
+                    <a href="/video" class="nav__link open-page">Video</a>
+                    <a href="#" class="nav__link open-popup">Popup</a>
+                </nav>
+            </div>
+        </header>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<style lang="scss" src="css/_base.scss"></style>
