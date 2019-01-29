23c8f0c0c585feacbb0758ba95a187aaaa01598f
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 11:27:15 +0800

    1.1.2: Add VueView Django view that renders Vue base template

--- demoproject/views.py
+++ demoproject/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'demoproject/index.html'
+
+
+class VueView(TemplateView):
+    template_name = 'demoproject/vue-index.html'
