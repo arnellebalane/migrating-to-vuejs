0b3167f726a83f0a8d146f621cf4f914d0fa1c97
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 12:48:03 +0800

    1.8.1: Move header markup and styles into TheHeader component

--- demoproject/static/demoproject/vue/App.vue
+++ demoproject/static/demoproject/vue/App.vue
@@ -1,22 +1,18 @@
 <template>
     <div>
-        <header class="header">
-            <div class="header__content wrapper">
-                <a href="/" class="open-page">
-                    <h1 class="header__title">DemoProject</h1>
-                </a>
-
-                <nav class="nav">
-                    <a href="/image" class="nav__link open-page">Image</a>
-                    <a href="/gallery" class="nav__link open-page">Gallery</a>
-                    <a href="/video" class="nav__link open-page">Video</a>
-                    <a href="#" class="nav__link open-popup">Popup</a>
-                </nav>
-            </div>
-        </header>
-
+        <TheHeader />
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+    components: {
+        TheHeader
+    }
+};
+</script>
 
 <style lang="scss" src="css/_base.scss"></style>
---
+++ demoproject/static/demoproject/vue/components/TheHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <div class="wrapper">
+            <a href="/">
+                <h1>DemoProject</h1>
+            </a>
+
+            <nav>
+                <a href="/image">Image</a>
+                <a href="/gallery">Gallery</a>
+                <a href="/video">Video</a>
+                <a href="#">Popup</a>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    position: sticky;
+    top: 0;
+    border-bottom: 1px solid #eee;
+    background-color: #fff;
+}
+
+.wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 24px 0;
+}
+
+h1 {
+    font-size: 24px;
+}
+
+nav a:not(:first-child) {
+    margin-left: 18px;
+}
+</style>
