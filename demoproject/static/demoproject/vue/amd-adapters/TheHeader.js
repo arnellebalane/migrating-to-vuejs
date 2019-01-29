23d97074f98f782d22b0c5d6489f3a0253acfaa9
Arnelle Balane <arnellebalane@gmail.com>
2019-01-29 20:33:31 +0800

    5.5.1: Use custom VueRouter instance in TheHeader adapter

--- demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
+++ demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
-import router from '../router';
+import VueRouter from 'vue-router';
 import store from '../store';
 import TheHeader from '../components/TheHeader.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: []
+});
 
 export default new Vue({
     router,
