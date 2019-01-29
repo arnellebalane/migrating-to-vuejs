e8762d70c257c2f26ee14384dc2a1811d521bf12
Arnelle Balane <arnellebalane@gmail.com>
2019-01-30 00:22:41 +0800

    5.7.2: Add initial router-link mock

--- demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
+++ demoproject/static/demoproject/vue/amd-adapters/TheHeader.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import store from '../store';
 import TheHeader from '../components/TheHeader.vue';
 
+Vue.component('router-link', {
+    render(h) {
+        return h('a', {}, this.$slots.default);
+    }
+});
+
 export default new Vue({
     store,
     render: h => h(TheHeader)
